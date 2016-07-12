6147c6a9213893bdf79e78a7665ebd9acd1b1817
Tyler Rockwood <rockwotj@gmail.com>
2016-07-12 12:58:50 -0700

    Updating to new Rosefire and Angularfire SDKs

--- project.js
+++ project.js
@@ -1,56 +1,59 @@
 angular.module('project',['firebase', 'ngRoute', 'rosefire']) 
-  .value('fbURL', 'https://rockwotj-clipboard.firebaseio.com/')
-  .value('rfToken', 'e5daaf8793be0ef8d35f23bcfccd8c948bfa34f404f1702003a90587415b6ad19934911f2f2bf0c3a79ce29d813c0235qcj8r9KAk/QnAnwyElDxqJGkxPeKINbOp/PVYOLB1b6fB5i6n1DYfa6QCfonbJ/s9hqUoXHWDE/FrmtWN0RWmg==')
-  .service('fbRef', function(fbURL) {
-    return new Firebase(fbURL);
-  })
+  .value('rfToken', 'b0e87fa7-492f-4612-8e66-5fd633451a92')
   .config(function($routeProvider, $locationProvider) {
-  $routeProvider
-    .when('/login', {controller: LoginCtrl, templateUrl: 'login.html'})
-    .when('/', {controller: MainCtrl, templateUrl: 'clipboard.html'})
-    .otherwise('/');
-  $locationProvider.html5Mode(true);
-});
-
-function MainCtrl($scope, $firebaseAuth, $firebaseObject, fbRef, $location) {
-  var authObj = $firebaseAuth(fbRef);
-  if (!authObj.$getAuth()) {
-    $location.path('/login');
-    return;
-  }
-  var fireObj = $firebaseObject(fbRef);
-  $scope.logout = function() {
-    fireObj.$destroy();
-    authObj.$unauth();
-    $location.path('/login');
-  }
-  fireObj.$watch(function() {
-    $scope.text = fireObj.$value;
+    var config = {
+      apiKey: "AIzaSyBpj63v0o4gcQaICvI6kCMm8y8QkwQ4d7I",
+      authDomain: "rockwotj-clipboard.firebaseapp.com",
+      databaseURL: "https://rockwotj-clipboard.firebaseio.com",
+      storageBucket: "rockwotj-clipboard.appspot.com",
+    }; 
+    firebase.initializeApp(config);
+    $routeProvider
+      .when('/login', {controller: LoginCtrl, templateUrl: 'login.html'})
+      .when('/', {controller: MainCtrl, templateUrl: 'clipboard.html'})
+      .otherwise('/');
+    $locationProvider.html5Mode(true);
   });
-  if (authObj.$getAuth().uid === 'rockwotj') {
-    $scope.onChange = function() {
-      fireObj.$value = $scope.text;
-      console.log(fireObj.$value);
-      fireObj.$save()
-    }
-  } else {
-    $scope.onChange = angular.noop;
-  }
-}
 
-function LoginCtrl($scope, rfToken, fbRef, Rosefire, $firebaseAuth, $location) {
-  var authObj = $firebaseAuth(fbRef);
-  $scope.login = function() {
-    console.log('Logging in with rosefire..');
-    Rosefire.signIn(rfToken)
-      .then(function(token) {
-        return authObj.$authWithCustomToken(token);
-      })
-      .then(function(authData) {
-        $location.path('/');
-      })
-      .catch(function(err) {
-        console.error(err);    
-      });
-  };
-}
+  function MainCtrl($scope, $firebaseAuth, $firebaseObject, $location) {
+    var authObj = $firebaseAuth();
+    if (!authObj.$getAuth()) {
+      $location.path('/login');
+      return;
+    }
+    var fireObj = $firebaseObject(firebase.database().ref());
+    $scope.logout = function() {
+      fireObj.$destroy();
+      authObj.$signOut();
+      $location.path('/login');
+    }
+    fireObj.$watch(function() {
+      $scope.text = fireObj.$value;
+    });
+    if (authObj.$getAuth().uid === 'rockwotj') {
+      $scope.onChange = function() {
+        fireObj.$value = $scope.text;
+        console.log(fireObj.$value);
+        fireObj.$save()
+      }
+    } else {
+      $scope.onChange = angular.noop;
+    }
+  }
+
+  function LoginCtrl($scope, rfToken, Rosefire, $firebaseAuth, $location) {
+    var authObj = $firebaseAuth();
+    $scope.login = function() {
+      console.log('Logging in with rosefire..');
+      Rosefire.signIn(rfToken)
+        .then(function(rfUser) {
+          return authObj.$signInWithCustomToken(rfUser.token);
+        })
+        .then(function(authData) {
+          $location.path('/');
+        })
+        .catch(function(err) {
+          console.error(err);    
+        });
+    };
+  }
